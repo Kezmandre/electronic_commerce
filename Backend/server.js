4912d4af077070fe99ec0e5a9e5d07fba499fef2
Quadri <kazeemquadri75@gmail.com>
2024-01-11 10:04:33 +0000

    m

--- Backend/server.js
+++ Backend/server.js
@@ -58,7 +58,7 @@ dbConnect()
   .then((result) => {
     console.log(`connected to Database`.bgGreen);
 
-    const port = config.env === "production" ? config.env : 5000;
+    const port = config.env === "production" ? process.env.PORT : 5000;
     app.listen(port, (err) => {
       if (err) {
         console.log(`error:${err}`.bgRed);
